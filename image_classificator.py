cd5222ed424073dfb2736502e90cdfa68295c297
haimin777 <med53@mail.ru>
2018-06-12 17:40:14 +0300

    classes added

--- image_classificator.py
+++ image_classificator.py
@@ -8,65 +8,66 @@ Created on Tue May 29 18:42:54 2018
 """
 
 import os
-
-import numpy as np
 import matplotlib.pyplot as plt
 import pydicom
 import shutil as sh
 
 
-#укажите сортируемую папку                   
-def image_sort(sorting_dir = 'C:\\Users\\Haimin\\Dicom\\DICOM2'):
-    
-    os.chdir(sorting_dir)
-    for p1 in os.listdir(sorting_dir):
-        ds = pydicom.dcmread(p1)
-        if ds.ImageType[0] == 'ORIGINAL' or ds.ViewPosition == 'CC':
-            print("deleting ", p1)
-            os.remove(p1)
-        
-def recursive_list(dir_from = 'C:\\Users\\Haimin\\Dicom\\test',
-                   dir_to = 'C:\\Users\\Haimin\\Dicom\\DICOM2'):
-   n=1
-   for root, dirs, files in os.walk(dir_from):
-       for name in files:
-           print("copy {} to {}".format(name, n))
-           sh.copyfile(os.path.join(root, name),
-                       os.path.join(dir_to, str(n)))
-           n+=1
-           
-    
-def print_image(im):
-    ds = pydicom.dcmread(im)
-    plt.figure(figsize=(5, 10))  #displaying image size
-    plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
-    plt.ylabel(im)
-    plt.show()
+class ImageClassificator:
+
+    def __init__(self):
+        self.sorting_dir = 'C:\\Users\\Haimin\\Dicom\\DICOM2'
+        self.dir_from = 'C:\\Users\\Haimin\\Dicom\\test'
+        self.dir_to = 'C:\\Users\\Haimin\\Dicom\\DICOM2'
+        self.dir_to0 = 'C:\\Users\\Haimin\\Dicom\\01'
+        self.dir_to1 = 'C:\\Users\\Haimin\\Dicom\\11'
+
+    def image_sort(self):
+
+        os.chdir(self.sorting_dir)
+        for p1 in os.listdir(self.sorting_dir):
+            ds = pydicom.dcmread(p1)
+            if ds.ImageType[0] == 'ORIGINAL' or ds.ViewPosition == 'CC':
+                print("deleting ", p1)
+                os.remove(p1)
+
+    def recursive_list(self):
+        n = 1
+        for root, dirs, files in os.walk(self.dir_from):
+            for name in files:
+                print("copy {} to {}".format(name, n))
+                sh.copyfile(os.path.join(root, name),
+                            os.path.join(self.dir_to, str(n)))
+                n += 1
+
+    def print_image(self, im):
+
+        ds = pydicom.dcmread(im)
+        plt.figure(figsize=(5, 10))  # displaying image size
+        plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
+        plt.ylabel(im)
+        plt.show()
+
+    def image_classifier(self):
+
+        # print(os.listdir(dir_from))
+        os.chdir(self.dir_to)
+        # plt.ion()
+        for p1 in os.listdir(self.dir_to):
+            self.print_image(p1)
+            a = input('for image {} 0 - bad or 1 - good pics, 2 - I dont know: '.format(p1))
+            if a == '0':
+                print('sended to dir 0')
+                sh.copyfile(p1, os.path.join(self.dir_to0, p1))
 
 
-def image_classifier(dir_from='C:\\Users\\Haimin\\Dicom\\DICOM2',
-                     dir_to0 = 'C:\\Users\\Haimin\\Dicom\\01',
-                     dir_to1 ='C:\\Users\\Haimin\\Dicom\\11'):
-#print(os.listdir(dir_from))
-    os.chdir(dir_from)
-    #plt.ion()
-    for p1 in os.listdir(dir_from):
-        print_image(p1)
-        a = input('for image {} 0 - bad or 1 - good pics, 2 - I dont know: '.format(p1))
-        if a == '0':
-            print('sended to dir 0')
-            sh.copyfile(p1, os.path.join(dir_to0, p1))
-            plt.close()
-            
-        elif a == '1':
-            print('sended to dir 1')
-            sh.copyfile(p1, os.path.join(dir_to1,p1))
-        elif a =='2':
-            print('not classified')
-            
-        
+            elif a == '1':
+                print('sended to dir 1')
+                sh.copyfile(p1, os.path.join(self.dir_to1, p1))
+            elif a == '2':
+                print('not classified')
+
+
 if __name__ == "__main__":
-    
-    #recursive_list()
-    #image_sort()
-    image_classifier()   
+    sorting = ImageClassificator()
+    sorting.image_classifier()
--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def image_downsampling(im, work_dir='C:\\Users\\Haimin\\Dicom\\01'):
-    #os.chdir(work_dir)
+    # os.chdir(work_dir)
     ds = pyd.dcmread(im)
     data = ds.pixel_array
     data_downsampling = data[::8, ::8]
@@ -16,47 +16,43 @@ def image_downsampling(im, work_dir='C:\\Users\\Haimin\\Dicom\\01'):
     print('convert from {} to {}'.format(data.shape, data_downsampling.shape))
 
 
-
-#---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 #                         adding classification tags and reshaping images
-#---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 
+class Preprocessing:
 
-def preprocessing(work_dir='C:\\Users\\Haimin\\Dicom\\01'):
-    os.chdir(work_dir)
-    for name in os.listdir(work_dir):
-        ds = pyd.dcmread(name)
+    def __init__(self):
+        self.workdir = 'C:\\Users\\Haimin\\Dicom\\01'
+
+    def preprocessing(self):
+        os.chdir(self.workdir)
+        for name in os.listdir(self.workdir):
+            ds = pyd.dcmread(name)
         data = ds.pixel_array
-        data_downsampling = data[::8, ::8] #downsize in 8 times
+        data_downsampling = data[::8, ::8]  # downsize in 8 times
         ds.PixelData = data_downsampling.tostring()
         ds.Rows, ds.Columns = data_downsampling.shape
         ds.AccessionNumber = '01'
         pyd.dcmwrite(name, ds)
         print('tag added and reshaped to', name)
 
+    def show_tags(self, im):
+        os.chdir(self.workdir)
+        ds = pyd.dcmread(im)
+        print(ds.AccessionNumber, '\n', ds.pixel_array.shape)
+        # print(ds.dir('num'))
 
-def show_tags(im, work_dir='C:\\Users\\Haimin\\Dicom\\01'):
-    os.chdir(work_dir)
-    ds = pyd.dcmread(im)
-    print(ds.AccessionNumber, '\n', ds.pixel_array.shape)
-    #print(ds.dir('num'))
-
-
-def create_input(work_dir='C:\\Users\\Haimin\\Dicom\\01'):
-    os.chdir(work_dir)
-    array_list = list()
-    for image in os.listdir(work_dir):
-        ds = pyd.dcmread(image)
-        array_list.append(ds.pixel_array)
-    input_images = np.expand_dims(np.array(array_list), -1)
-    print(input_images.shape)
+    def create_input(self):
+        os.chdir(self.workdir)
+        array_list = list()
+        for image in os.listdir(self.workdir):
+            ds = pyd.dcmread(image)
+            array_list.append(ds.pixel_array)
+        input_images = np.expand_dims(np.array(array_list), -1)
+        print(input_images.shape)
 
 
-if __name__=="__main__":
-    #os.chdir('C:\\Users\\Haimin\\Dicom\\01')
-    #pixel_array('23')
-    #image_downsampling('7')
-    #ic.print_image('7')
-    #preprocessing()
-    #show_tags('24')
-    create_input()
+if __name__ == "__main__":
+    start = Preprocessing()
+    start.create_input()
